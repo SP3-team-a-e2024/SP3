e4e7ba034e44ce3c68218f0d0109395422994424
Thomas Atchapero <t.atchapero@gmail.com>
2024-11-22 11:34:56 +0100

    Added User class and unit tests

--- src/media/Season.java
+++ src/media/Season.java
@@ -1,5 +1,7 @@
 package media;
 
+import enums.SeriesCategories;
+
 import java.util.Set;
 import java.util.TreeSet;
 
@@ -7,4 +9,8 @@ import java.util.TreeSet;
 
 public class Season extends Series {
     Set <Episode> episodes = new TreeSet<>();
+
+    public Season(String name, float rating, int releaseYear, Set<SeriesCategories> categories) {
+        super(name, rating, releaseYear, categories);
+    }
 }
--- src/media/Series.java
+++ src/media/Series.java
@@ -1,8 +1,44 @@
 package media;
 
+import enums.SeriesCategories;
+
 import java.util.Set;
 import java.util.TreeSet;
 
 public class Series extends Media {
     Set<Season> seasons = new TreeSet<>();
+
+    public Series(String name, float rating, int releaseYear, Set<SeriesCategories> categories) {
+        super(name, rating, releaseYear, categories);
+    }
+
+    @Override
+    public String getName() {
+        return "";
+    }
+
+    @Override
+    public float getRating() {
+        return 0;
+    }
+
+    @Override
+    public int getReleaseYear() {
+        return 0;
+    }
+
+    @Override
+    public Set<Enum> getCategories() {
+        return Set.of();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return 0;
+    }
 }
