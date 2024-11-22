0df1a9e32635bb8911267412a103574644bf32e3
Frederik-BipBop <Bastiansen44@gmail.com>
2024-11-22 11:14:38 +0100

    Making FileIO changing Savecredentials from ArrayList to HashMap

---
+++ src/util/FileIO.java
@@ -0,0 +1,49 @@
+package util;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.*;
+
+public class FileIO {
+
+        public static HashMap<String, String> readUserCredentials(String path) {
+            HashMap<String, String> data = new HashMap<String, String>();
+            File file = new File(path);
+            try {
+                Scanner scan = new Scanner(file);
+                scan.nextLine();//skip header
+
+                while (scan.hasNextLine()) {
+                    String line = scan.nextLine(); // "tess, 40000"
+                    String[] lineSplitter = line.split(";");
+                    data.put(lineSplitter[0].trim(), lineSplitter[1].trim());
+                }
+            } catch (FileNotFoundException e) {
+                System.out.println("File was not found");
+            }
+            return data;
+        }
+
+
+        public static void saveCredentials(String username, String password, String path) {
+
+            try {
+                File file = new File(path);
+
+               HashMap<String , String> credentials = readUserCredentials(path);
+               credentials.put(username,  password);
+                FileWriter writer = new FileWriter(path);
+                for(Map.Entry<String , String> entry : credentials.entrySet()) {
+                    writer.write(entry.getKey() + ";" + entry.getValue()+ "\n");
+                }
+                writer.close();
+            } catch (IOException e) {
+                System.out.println("something went wrong when writing to file");
+            }
+        }
+        public void readMedia(){
+
+        }
+}
