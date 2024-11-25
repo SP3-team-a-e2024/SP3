c45e982d728bfb4d4d2cc400f4a3f0ea854db02c
Iirne <113694737+Iirne@users.noreply.github.com>
2024-11-25 09:15:47 +0100

    updated main to make code run

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -2,6 +2,7 @@ package Main;
 
 public class Main {
     public static void main(String[] args) {
-
+        StreamingService service = new StreamingService();
+        service.StreamingService();
     }
 }
